11db53b0268b0b1de32708c8bead64ba654ef583
Dmitry <levin1581986@gmail.com>
2022-09-20 10:15:01 +0300

    new commit 20/09/2022

--- src/com/levin/Main.java
+++ src/com/levin/Main.java
@@ -3,7 +3,8 @@ package com.levin;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println();
         System.out.println();
     }
 }
