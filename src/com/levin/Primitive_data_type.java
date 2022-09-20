9cd9bb1914412800c3d68d66fbf906c0eee50e74
Dmitry <levin1581986@gmail.com>
2022-09-20 13:55:29 +0300

    primitive data type 20/09/2022

--- src/com/levin/Primitive_data_type.java
+++ src/com/levin/Primitive_data_type.java
@@ -1,13 +1,14 @@
 package com.levin;
 
-public class New_class {
+public class Primitive_data_type {
     byte a = 127; // -128 * 128 (8 bit)
     short b = 32_767; // -32768 * 32768 (16 bit)
     int c = 2_147_483_647; // -2147483648 * 2147483648 (32 bit)
     long d = 9_223_372_036_854_775_807L; // - 9_223_372_036_854_775_808 * 9_223_372_036_854_775_808 (64 bit)
-    float i = 123.456F ; // (32 bit)
-    double f = 123.456 ; // (64 bit)
+    float i = 123.456F; // (32 bit)
+    double f = 123.456; // (64 bit)
     char j = 'f'; //  65_535 (16 bit)
-    boolean h = true ; // true/false
+    boolean h = true; // true/false
+    public class
 }
 
